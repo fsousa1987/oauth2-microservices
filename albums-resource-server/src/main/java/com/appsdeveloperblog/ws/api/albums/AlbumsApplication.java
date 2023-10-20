01a24e970d7f55c5d3c317bb8ba79eb4fb0e2c70
Francisco Sousa <franciscoeds1987@gmail.com>
2023-10-20 10:27:12 -0300

    solution overview

--- albums-resource-server/src/main/java/com/appsdeveloperblog/ws/api/albums/AlbumsApplication.java
+++ albums-resource-server/src/main/java/com/appsdeveloperblog/ws/api/albums/AlbumsApplication.java
@@ -2,12 +2,14 @@ package com.appsdeveloperblog.ws.api.albums;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 
 @SpringBootApplication
+@EnableDiscoveryClient
 public class AlbumsApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(AlbumsApplication.class, args);
-	}
+    public static void main(String[] args) {
+        SpringApplication.run(AlbumsApplication.class, args);
+    }
 
 }
