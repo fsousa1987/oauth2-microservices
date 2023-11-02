f3959909d2cb56a12163ffb09500facc667aef6d
Francisco Sousa <franciscoeds1987@gmail.com>
2023-11-02 07:04:23 -0300

    configure logout functionality

--- social-login-web-client/src/main/java/com/appsdeveloperblog/ws/clients/socialloginwebclient/security/WebSecurity.java
+++ social-login-web-client/src/main/java/com/appsdeveloperblog/ws/clients/socialloginwebclient/security/WebSecurity.java
@@ -5,12 +5,20 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.oauth2.client.oidc.web.logout.OidcClientInitiatedLogoutSuccessHandler;
+import org.springframework.security.oauth2.client.registration.ClientRegistrationRepository;
 import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.security.web.util.matcher.AntPathRequestMatcher;
 
 @Configuration
 @EnableWebSecurity
 public class WebSecurity {
+
+    final ClientRegistrationRepository clientRegistrationRepository;
+
+    public WebSecurity(ClientRegistrationRepository clientRegistrationRepository) {
+        this.clientRegistrationRepository = clientRegistrationRepository;
+    }
 
     @Bean
     public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
@@ -22,9 +30,24 @@ public class WebSecurity {
                                 .anyRequest()
                                 .authenticated()
                 )
-                .oauth2Login(Customizer.withDefaults());
+                .oauth2Login(Customizer.withDefaults())
+                .logout(
+                        logout -> logout
+                                .logoutSuccessHandler(oidcLogoutSuccessHandler())
+                                .invalidateHttpSession(true)
+                                .clearAuthentication(true)
+                                .deleteCookies("JSESSIONID")
+                );
 
         return http.build();
 
     }
+
+    private OidcClientInitiatedLogoutSuccessHandler oidcLogoutSuccessHandler() {
+        OidcClientInitiatedLogoutSuccessHandler successHandler =
+                new OidcClientInitiatedLogoutSuccessHandler(clientRegistrationRepository);
+        successHandler.setPostLogoutRedirectUri("http://localhost:8080/");
+        return successHandler;
+    }
+
 }
